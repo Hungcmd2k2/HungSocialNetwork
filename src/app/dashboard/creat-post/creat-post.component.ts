9c102c076d02db79f6d2d043080e5d52d6c8f961
Hungcmd <hungdang16042002@gmail.com>
2024-09-22 18:23:26 +0700

    22/9 night

--- src/app/dashboard/creat-post/creat-post.component.ts
+++ src/app/dashboard/creat-post/creat-post.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrl: './creat-post.component.css'
 })
 export class CreatPostComponent {
+  fileInfo: string[] = []; // Mảng lưu thông tin tệp
 
+    onFileSelected(event: Event): void {
+        const input = event.target as HTMLInputElement;
+        this.fileInfo = []; // Xóa thông tin cũ
+
+        if (input.files) {
+            for (let i = 0; i < input.files.length; i++) {
+                const file = input.files[i];
+                this.fileInfo.push(`Tên tệp: ${file.name}, Kích thước: ${file.size} bytes, Kiểu: ${file.type}`);
+            }
+        }
+    }
 }
